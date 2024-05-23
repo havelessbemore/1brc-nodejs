54c0d04960dd005681b26850c5dc6a510366063b
havelessbemore <dev.michael.rojas@gmail.com>
2024-05-23 10:30:20 -0400

    Add utility functions for working with Workers

---
+++ src/main/nodejs/havelessbemore/src/utils/worker.ts
@@ -0,0 +1,39 @@
+import { Worker } from "worker_threads";
+
+/**
+ * Creates a new Worker instance.
+ *
+ * @param workerPath - The path to the worker script.
+ *
+ * @returns A new Worker instance.
+ */
+export function createWorker(workerPath: string): Worker {
+  const worker = new Worker(workerPath);
+  worker.on("error", (err) => {
+    throw err;
+  });
+  worker.on("messageerror", (err) => {
+    throw err;
+  });
+  worker.on("exit", (code) => {
+    if (code > 1 || code < 0) {
+      throw new Error(`Worker ${worker.threadId} exited with code ${code}`);
+    }
+  });
+  return worker;
+}
+
+/**
+ * Executes a task on a Worker and returns a Promise that resolves with the response.
+ *
+ * @param worker - The Worker instance to execute the task.
+ * @param req - The request to send to the worker.
+ *
+ * @returns A Promise that resolves with the response from the worker.
+ */
+export function exec<Req, Res>(worker: Worker, req: Req): Promise<Res> {
+  return new Promise<Res>((resolve) => {
+    worker.once("message", resolve);
+    worker.postMessage(req);
+  });
+}
