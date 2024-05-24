fc8ed7dc9714c2177b83908392ae9125978b8688
havelessbemore <dev.michael.rojas@gmail.com>
2024-05-24 15:31:26 -0400

    Format files

--- src/main/nodejs/havelessbemore/src/utils/parse.ts
+++ src/main/nodejs/havelessbemore/src/utils/parse.ts
@@ -5,7 +5,7 @@ export const CHAR_ZERO_111 = 111 * CHAR_ZERO;
 
 /**
  * Converts an ASCII numeric string into an integer.
- * 
+ *
  * Fastest.
  */
 export function parseDouble(b: Buffer, min: number, max: number): number {
@@ -22,23 +22,23 @@ export function parseDouble(b: Buffer, min: number, max: number): number {
 
 /**
  * Converts an ASCII numeric string into an integer.
- * 
+ *
  * Second fastest.
  */
 export function parseDoubleFlat(b: Buffer, min: number, max: number): number {
-    const sign = -(b[min] === CHAR_MINUS);
-    b[min + ~sign] = CHAR_ZERO;
-    return (
-      ((100 * b[max - 4] + 10 * b[max - 3] + b[max - 1] - CHAR_ZERO_111) ^ sign) -
-      sign
-    );
-  }
+  const sign = -(b[min] === CHAR_MINUS);
+  b[min + ~sign] = CHAR_ZERO;
+  return (
+    ((100 * b[max - 4] + 10 * b[max - 3] + b[max - 1] - CHAR_ZERO_111) ^ sign) -
+    sign
+  );
+}
 
 /**
  * Converts an ASCII numeric string into an integer without branching.
  *
  * Inspired by {@link https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_thomaswue.java#L68 | Quan Anh Mai's method}.
- * 
+ *
  * Slowest.
  */
 export function parseDoubleQuan(b: Buffer, min: number, max: number): number {
