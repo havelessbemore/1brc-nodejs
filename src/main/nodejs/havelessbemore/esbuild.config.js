a5cd0c815e60e10494f3efff332b489dfa679e04
havelessbemore <dev.michael.rojas@gmail.com>
2024-05-25 12:51:48 -0400

    Change builder from rollup to esbuild

---
+++ src/main/nodejs/havelessbemore/esbuild.config.js
@@ -0,0 +1,20 @@
+import * as esbuild from "esbuild";
+
+import pkg from "./package.json" with { type: "json" };
+
+/** @type {import('esbuild').BuildOptions} */
+const options = {
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  drop: ["console"],
+  minify: false,
+  platform: "node",
+  sourcemap: true,
+  target: "ESNext",
+};
+
+await esbuild.build({
+  ...options,
+  format: "esm",
+  outfile: pkg.module,
+});
