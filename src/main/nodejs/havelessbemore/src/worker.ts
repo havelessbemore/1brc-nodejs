90433574e8d1bc888923badc5076d67cf2b36e1e
havelessbemore <dev.michael.rojas@gmail.com>
2024-05-27 01:44:18 -0400

    Refactor worker

--- src/main/nodejs/havelessbemore/src/worker.ts
+++ src/main/nodejs/havelessbemore/src/worker.ts
@@ -22,6 +22,21 @@ export function run({
   mins,
   sums,
 }: ProcessRequest): ProcessResponse {
+  const newStation = (index: number, temp: number): void => {
+    mins[index << 3] = temp;
+    maxes[index << 3] = temp;
+    counts[index << 2] = 1;
+    sums[index << 1] = temp;
+  };
+
+  const updateStation = (index: number, temp: number): void => {
+    index <<= 3;
+    mins[index] = mins[index] <= temp ? mins[index] : temp;
+    maxes[index] = maxes[index] >= temp ? maxes[index] : temp;
+    ++counts[index >> 1];
+    sums[index >> 2] += temp;
+  };
+
   // Initialize constants
   const chunkSize = getHighWaterMark(end - start);
   const chunk = Buffer.allocUnsafe(chunkSize + BRC.MAX_ENTRY_LEN);
@@ -36,38 +51,41 @@ export function run({
   // For each chunk
   while (start < end) {
     // Read the chunk into memory
-    const bytesRead = readSync(fd, chunk, bufI, chunkSize, start);
+    const bytesRead = Math.min(chunkSize, end - start);
+    readSync(fd, chunk, bufI, bytesRead, start);
     start += bytesRead;
 
     // For each byte
     for (const N = bufI + bytesRead; bufI < N; ++bufI) {
-      // If newline
-      if (chunk[bufI] === CharCode.NEWLINE) {
-        // Get semicolon
-        let semI = bufI - 5;
-        if (chunk[semI] !== CharCode.SEMICOLON) {
-          semI += 1 | (1 + ~(chunk[semI - 1] === CharCode.SEMICOLON));
-        }
+      // If not newline
+      if (chunk[bufI] !== CharCode.NEWLINE) {
+        continue;
+      }
 
-        // Add the station's name to the trie and get leaf
-        [trie, leaf] = add(trie, chunk, minI, semI);
+      // Get semicolon
+      let semI = bufI - 5;
+      if (chunk[semI] !== CharCode.SEMICOLON) {
+        semI += 1 | (1 + ~(chunk[semI - 1] === CharCode.SEMICOLON));
+      }
 
-        // Update next entry's min
-        minI = bufI + 1;
+      // Add the station's name to the trie and get leaf
+      [trie, leaf] = add(trie, chunk, minI, semI);
 
-        // Get temperature
-        const temp = parseDouble(chunk, semI + 1, bufI);
+      // Update next entry's min
+      minI = bufI + 1;
 
-        // If the station existed
-        leaf += TrieNodeProto.VALUE_IDX;
-        if (trie[leaf] !== Trie.NULL) {
-          // Update the station's value
-          updateStation(trie[leaf], temp);
-        } else {
-          // Add the new station's value
-          trie[leaf] = ++stations;
-          newStation(stations, temp);
-        }
+      // Get temperature
+      const temp = parseDouble(chunk, semI + 1, bufI);
+
+      // If the station existed
+      leaf += TrieNodeProto.VALUE_IDX;
+      if (trie[leaf] !== Trie.NULL) {
+        // Update the station's value
+        updateStation(trie[leaf], temp);
+      } else {
+        // Add the new station's value
+        trie[leaf] = ++stations;
+        newStation(stations, temp);
       }
     }
 
@@ -77,21 +95,6 @@ export function run({
     // Update indices for the next chunk
     bufI -= minI;
     minI = 0;
-  }
-
-  function newStation(index: number, temp: number): void {
-    mins[index << 3] = temp;
-    maxes[index << 3] = temp;
-    counts[index << 2] = 1;
-    sums[index << 1] = temp;
-  }
-
-  function updateStation(index: number, temp: number): void {
-    index <<= 3;
-    mins[index] = mins[index] <= temp ? mins[index] : temp;
-    maxes[index] = maxes[index] >= temp ? maxes[index] : temp;
-    ++counts[index >> 1];
-    sums[index >> 2] += temp;
   }
 
   return { id, trie };
@@ -106,14 +109,13 @@ export function merge({
   mins,
   sums,
 }: MergeRequest): MergeResponse {
-  function mergeStations(ai: number, bi: number): void {
+  const ids = mergeLeft(tries, a, b, (ai: number, bi: number): void => {
     ai <<= 3;
     bi <<= 3;
     mins[ai] = mins[ai] <= mins[bi] ? mins[ai] : mins[bi];
-    maxes[ai] = mins[ai] >= mins[bi] ? mins[ai] : mins[bi];
+    maxes[ai] = maxes[ai] >= maxes[bi] ? maxes[ai] : maxes[bi];
     counts[ai >> 1] += counts[bi >> 1];
     sums[ai >> 2] += sums[bi >> 2];
-  }
-  const ids = mergeLeft(tries, a, b, mergeStations);
+  });
   return { ids, tries };
 }
